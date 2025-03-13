d2bc5c1440c2af409f2cbfea749d4381dcc157f0
itzmonikak <163900650+itzmonikak@users.noreply.github.com>
2025-03-13 22:17:02 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout<<"changes made\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
